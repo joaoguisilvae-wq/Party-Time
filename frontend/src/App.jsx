2d62ff34b4f25e31325588fbc38edec4f96b7755
João <joaogui.silvae@gmail.com>
2026-01-29 06:49:04 -0300

    Termino do back end do projeto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import "./App.css";
 
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ToastContainer />
+      <NavBar />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
